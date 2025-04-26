83830ec0121694b813c319cce3052ce6789c4d72
Denis <denispuscas1@gmail.com>
2025-04-26 16:03:00 +0300

    feat: input validation for login and registration refs: #7

--- Backend/travel/src/main/java/com/travel/dtos/LoginRequestDTO.java
+++ Backend/travel/src/main/java/com/travel/dtos/LoginRequestDTO.java
@@ -1,9 +1,17 @@
 package com.travel.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class LoginRequestDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 }
--- Backend/travel/src/main/java/com/travel/dtos/UserRequestDTO.java
+++ Backend/travel/src/main/java/com/travel/dtos/UserRequestDTO.java
@@ -1,11 +1,27 @@
 package com.travel.dtos;
 
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserRequestDTO {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    @Pattern(regexp = "^[A-Z][a-zA-Z ]*$", message = "Name must start with an uppercase letter and contain only letters and spaces")
     private String name;
+
+    @Email(message = "Invalid email")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @Pattern(
+            regexp = "^(?=.*\\d)[A-Z][a-zA-Z\\d]{5,}$",
+            message = "Password must start with an uppercase letter, be at least 6 characters, and contain at least one number"
+    )
     private String password;
     private boolean admin;
 }
