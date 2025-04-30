cdec38a255b762c887cc66fc7886d41fa2ef7565
Ivan Alexandru <alexandru_ivan91@yahoo.com>
2025-04-30 18:33:22 +0300

    fix: updated branch

--- Frontend/travel/src/App.tsx
+++ Frontend/travel/src/App.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Registration from "./pages/user/registration";
 import Login from "./pages/user/login";
 import AttractionDetail from "./pages/attractions/attractionsDetails";
+import Attractions from "./pages/attractions/attractions";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
-          <Route path="/attractions/:id" element={<AttractionDetail />} />
-
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/register" element={<Registration/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/attractions/:id" element={<AttractionDetail/>}/>
+                <Route path="/attractions" element={<Attractions/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
