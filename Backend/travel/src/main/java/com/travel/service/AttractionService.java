c92f93d5d3c072fe230ce2322c014d5737e1a78d
vladvadean <vadean.vlad23@gmail.com>
2025-05-12 23:06:49 +0300

    fix: updates the old price and shows the discount only if the old price is higher than the price

--- Backend/travel/src/main/java/com/travel/service/AttractionService.java
+++ Backend/travel/src/main/java/com/travel/service/AttractionService.java
@@ -97,8 +97,11 @@ public class AttractionService {
     }
 
     public AttractionResponseDTO updateAttraction(Long id, AttractionRequestDTO attractionRequestDTO) {
+        Optional<AttractionEntity> originalEntity = attractionRepository.findAttractionById(id);
         AttractionEntity attractionEntity = attractionMapper.toEntity(attractionRequestDTO);
         attractionEntity.setId(id);
+        System.out.println("!!" + originalEntity.get().getPrice());
+        attractionEntity.setOldPrice(originalEntity.get().getPrice());
         return attractionMapper.toDTO(attractionRepository.save(attractionEntity));
     }
 
