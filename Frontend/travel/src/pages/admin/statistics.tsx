29bcaae41ec65184cdf9a6520ced92c55c89237a
Denis <denispuscas1@gmail.com>
2025-05-16 00:01:12 +0300

    fix: add card title

--- Frontend/travel/src/pages/admin/statistics.tsx
+++ Frontend/travel/src/pages/admin/statistics.tsx
@@ -7,18 +7,18 @@ export default function Statistics() {
     <div className="statisticsPage">
       <Row className="statisticsRow" gutter={[30, 30]}>
         <Col xs={24} md={11}>
-          <Card className="statisticsCard" title="Titlu Chart 1"></Card>
+          <Card className="statisticsCard" title="Hourly Visits"></Card>
         </Col>
         <Col xs={24} md={11}>
-          <Card className="statisticsCard" title="Titlu Chart 2"></Card>
+          <Card className="statisticsCard" title="Monthly Visits"></Card>
         </Col>
       </Row>
       <Row className="statisticsRow" gutter={[30, 30]}>
         <Col xs={24} md={11}>
-          <Card className="statisticsCard" title="Titlu Chart 3"></Card>
+          <Card className="statisticsCard" title="Top Attractions"></Card>
         </Col>
         <Col xs={24} md={11}>
-          <Card className="statisticsCard" title="Titlu Chart 4"></Card>
+          <Card className="statisticsCard" title="Top Locations"></Card>
         </Col>
       </Row>
     </div>
