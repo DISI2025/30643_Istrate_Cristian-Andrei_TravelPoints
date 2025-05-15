d23cbea2378fe5944d9d7c6b04d4abeb227c5152
Denis <denispuscas1@gmail.com>
2025-05-15 22:41:36 +0300

    fix: remove constructor and add @AllArgsConstructor

--- Backend/travel/src/main/java/com/travel/dtos/VisitTopStatsResponseDTO.java
+++ Backend/travel/src/main/java/com/travel/dtos/VisitTopStatsResponseDTO.java
@@ -1,16 +1,13 @@
 package com.travel.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class VisitTopStatsResponseDTO {
     private List<VisitTopAttractionDTO> topAttractions;
     private List<VisitTopLocationDTO> topLocations;
-
-    public VisitTopStatsResponseDTO(List<VisitTopAttractionDTO> topAttractions, List<VisitTopLocationDTO> topLocations) {
-        this.topAttractions = topAttractions;
-        this.topLocations = topLocations;
-    }
 }
